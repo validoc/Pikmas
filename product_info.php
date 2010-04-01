0c4cad944010818a6d18149d7a0169f5a0ec6c21
elmagodexter <maquetador.freelance@gmail.com>
2010-04-01 01:31:44 -0300

    menu hover

--- product_info.php
+++ product_info.php
@@ -261,11 +261,8 @@
 
 </tr>
 <script type="text/javascript">
-	<!--
 var TabbedPanels1 = new Spry.Widget.TabbedPanels("TabbedPanels1");
 var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
-//-->
-
 </script>
 
 </body>
